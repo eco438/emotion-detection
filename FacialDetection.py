76541b5d194ace01300caf66c8b2cfea6e6b0f28
eco438 <oka4emmanuel97@gmail.com>
2020-04-15 19:44:56 -0400

    Removed the other code and updated my tracker

--- FacialDetection.py
+++ FacialDetection.py
@@ -2,6 +2,8 @@ import face_recognition
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -12,42 +14,50 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
-cclass_labels = ['angry','happy','neutral','sad','surprise']
+classifier =load_model('Emotion_model.h5')
 
+class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+    rgb_frame = frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-        
-    process_this_frame = not process_this_frame
-
-
-    # Display the results
-    for (top, right, bottom, left) in face_locations :
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+       
 
         # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 5)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        
+        roi_gray = gray[top:bottom,left:right]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+        # Draw a label with a name below the face
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+        # make a prediction on the ROI, then lookup the class
+
+            preds = classifier.predict(roi)[0]
+            label=class_labels[preds.argmax()]
+            label_position = (left,top)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        else:
+            cv2.putText(frame,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+    cv2.imshow('Emotion Detector',frame)
+       
+
     # Display the resulting image
-    cv2.imshow('Video', frame)
-    
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
