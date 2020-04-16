5c0ddd7f535870c4d4a51ca34b63257df6c3275a
eco438 <oka4emmanuel97@gmail.com>
2020-04-15 20:27:19 -0400

    Cleaned up code

--- FacialDetection.py
+++ FacialDetection.py
@@ -1,30 +1,24 @@
 import face_recognition
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
-
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
-face_names = []
-process_this_frame = True
+
 
 classifier =load_model('Emotion_model.h5')
-
 emotion_dict = {0: "Angry",  2: "Happy", 3: "Neutral", 4: "Sad", 5: "Surprised"}
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
-
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     (H, W) = frame.shape[:2]
-
     rgb_frame = frame[:, :, ::-1]
 
     # Find all the faces and face enqcodings in the frame of video
@@ -33,17 +27,12 @@ while True:
 
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-       
-
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255,0), 2)
-        
         roi_gray = gray[top:bottom,left:right]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
         # Draw a label with a name below the face
         if np.sum([roi_gray])!=0:
-
         # make a prediction on the ROI, then lookup the class
             cropped_img = np.expand_dims(np.expand_dims(roi_gray,-1),0)
             preds = classifier.predict(cropped_img)
@@ -53,14 +42,10 @@ while True:
         else:
             cv2.putText(frame,'No Face Found',(10,H),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
     cv2.imshow('Emotion Detector',frame)
-       
-
-    # Display the resulting image
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
